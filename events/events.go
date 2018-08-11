22d7a9f5bc23425a09773df0bd4fb8a91c3ea4a8
penggy <peng.wu@foxmail.com>
2018-08-11 10:23:05 +0800

    add events package

---
+++ events/events.go
@@ -0,0 +1,62 @@
+package events
+
+import "sync"
+
+type EventListener struct {
+	Name       string
+	HandleFunc func(...interface{}) bool
+}
+
+func NewEventListener(name string, f func(...interface{}) bool) (el *EventListener) {
+	el = &EventListener{
+		Name:       name,
+		HandleFunc: f,
+	}
+	return
+}
+
+type EventDispatcher struct {
+	listeners     map[*EventListener]bool
+	listenersLock *sync.RWMutex
+}
+
+func NewEventDispatcher() (ed *EventDispatcher) {
+	ed = &EventDispatcher{
+		listeners:     make(map[*EventListener]bool),
+		listenersLock: &sync.RWMutex{},
+	}
+	return
+}
+
+func (ed *EventDispatcher) AddListener(el *EventListener) {
+	ed.listenersLock.Lock()
+	defer ed.listenersLock.Unlock()
+	ed.listeners[el] = true
+}
+
+func (ed *EventDispatcher) RemoveListener(el *EventListener) {
+	ed.listenersLock.Lock()
+	defer ed.listenersLock.Unlock()
+	delete(ed.listeners, el)
+}
+
+func (ed *EventDispatcher) GetListeners(name string) (els []*EventListener) {
+	els = make([]*EventListener, 0)
+	ed.listenersLock.RLock()
+	defer ed.listenersLock.RUnlock()
+	for el, _ := range ed.listeners {
+		if el.Name == name {
+			els = append(els, el)
+		}
+	}
+	return
+}
+
+func (ed *EventDispatcher) Trigger(name string, msg ...interface{}) {
+	els := ed.GetListeners(name)
+	for _, el := range els {
+		if !el.HandleFunc(msg) {
+			break
+		}
+	}
+}
