cb4111fb6836a7b6b598f3a5035ce73719126f84
penggy <peng.wu@foxmail.com>
2018-08-28 09:36:35 +0800

    update redirect stderr os.O_APPEND

--- utils/log_windows.go
+++ utils/log_windows.go
@@ -23,7 +23,7 @@ func setStdHandle(stdhandle int32, handle syscall.Handle) error {
 
 // RedirectStderr to the file passed in
 func RedirectStderr() (err error) {
-	logFile, err := os.OpenFile(ErrorLogFilename(), os.O_WRONLY|os.O_CREATE|os.O_SYNC, 0644)
+	logFile, err := os.OpenFile(ErrorLogFilename(), os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0644)
 	if err != nil {
 		return
 	}
